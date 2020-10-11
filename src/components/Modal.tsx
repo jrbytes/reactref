7d0b6e73e61b9d544ee39b61064afba1a5df5009
Je Bytes <itamazor@gmail.com>
2020-10-11 19:35:25 -0300

    :tada: CodeDrops - useRef and useImperativeHandle.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,37 @@
+import React, { forwardRef, useCallback, useState, useImperativeHandle } from 'react'
+
+export interface ModalHandles {
+  openModal: () => void
+}
+
+const Modal: React.RefForwardingComponent<ModalHandles> = (props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const openModal = useCallback(() => {
+    setVisible(true)
+  }, [])
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal
+    }
+  })
+
+  const handleCloseModal = useCallback(() => {
+    setVisible(false)
+  }, [])
+  
+  if (!visible) {
+    return null
+  }
+
+  return (
+    <div>
+      Modal aberto
+
+      <button onClick={handleCloseModal}>Fechar Modal</button>
+    </div>
+  )
+}
+
+export default forwardRef(Modal)
